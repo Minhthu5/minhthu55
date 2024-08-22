2e0bc582d2c80a3330cd7df8e9659f430c75f75e
Minhthu5 <immthu5227@gmail.com>
2024-08-22 19:01:23 +0700

    Add files via upload

---
+++ PT bac 2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 18:58:41 2024
+
+@author: MinhThu
+"""
+
+import math
+a = float(input("nhập a: "))
+b = float(input("nhập b: "))
+c = float(input("nhập c: "))
+print("{0}x^2 + {1}x + {2} = 0".format(a,b,c))
